04284cae9b07e0024ecc5af4e0a689781fa2772c
Agathe Gossey <agathe.gossey@gmail.com>
2019-02-11 22:46:51 +0100

    add data base export

--- my_manager.sql
+++ my_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 04 fév. 2019 à 23:10
+-- Généré le :  lun. 11 fév. 2019 à 22:42
 -- Version du serveur :  10.1.37-MariaDB
 -- Version de PHP :  7.2.12
 
@@ -44,8 +44,9 @@ CREATE TABLE `budget` (
 --
 
 INSERT INTO `budget` (`id`, `date_budget`, `name`, `mode`, `category`, `reason`, `type`, `amount`) VALUES
-(55, '2019-02-21 23:00:00', 'sdfsdfs', 'ChÃ¨que', '46', 'sdfsf', 'Recette', 50),
-(56, '2019-01-30 23:00:00', 'd', 'Virement', '46', 'd', 'Recette', 10);
+(60, '2019-01-31 23:00:00', 'Agathe', 'ChÃ¨que', '4', 'Site du samedi', 'Depense', 250),
+(61, '2019-02-06 23:00:00', 'Vincent', 'Virement', '3', 'De Lasst', 'Depense', 1000),
+(62, '2019-02-20 23:00:00', 'CÃ©cile', 'Virement', '6', 'DÃ©partement', 'Recette', 15000);
 
 -- --------------------------------------------------------
 
@@ -63,8 +64,10 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name_category`) VALUES
-(46, 'dfgdfg'),
-(49, 'mklmklmkm');
+(3, 'Concert'),
+(4, 'DÃ©coration'),
+(5, 'Catering'),
+(6, 'Subvention');
 
 -- --------------------------------------------------------
 
@@ -79,15 +82,19 @@ CREATE TABLE `users` (
   `last_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `approved` int(11) NOT NULL
+  `approved` int(11) NOT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `password`, `email`, `approved`) VALUES
-(22, 'hhh', 'hh', 'hh', '$2y$10$ExRUYGM3zfqyegOyWcSOFelwxeSukwrjM.HG6uz7yQPTV9/K3KgGm', 'agathe@gmail.com', 0);
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `password`, `email`, `approved`, `status`) VALUES
+(26, 'Admin', 'Admin', 'Admin', '$2y$10$jCeFrqdhByTaibNLh3hT1uNtuVR2Urfj0ORv4WRo26KIB73HNMq7e', 'admin@admin.admin', 1, 1),
+(35, 'Agathe', 'Agathe', 'Gossey', '$2y$10$o35du1XM34Lcs1S15eA0B.8/nJ0gI9.0TSlLOTq.XaKx9Ux.5ZMr6', 'aga@de.fr', 1, 0),
+(36, 'CÃ©cile', 'CÃ©cile', 'Deq', '$2y$10$7on9dm3VmTYdCkvqF8GK9.3udO.x4.OdTWg9xIW3YO7lzpFQ2m3Vq', 'cecile@gmail.com', 0, 0),
+(37, 'Vince', 'Vincent', 'Lar', '$2y$10$S9qNVYrpkLYkeEk3ru3IjOsVUzDzU1m.sVl8zOjlDoLdI9SlnCHHO', 'vincent@gmail.com', 0, 0);
 
 --
 -- Index pour les tables déchargées
@@ -109,7 +116,8 @@ ALTER TABLE `category`
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -119,19 +127,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `budget`
 --
 ALTER TABLE `budget`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
